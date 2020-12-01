395395f335148a17e990300118aec89904d70992
Giorgio Pomettini <giorgio.freespire@gmail.com>
2020-12-01 12:22:36 +0000

    Updated day one

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -5,20 +5,23 @@ fn main() {
 
     let numbers: Vec<i64> = input
         .lines()
-        .map(|line| line.parse().expect("Number is not valid"))
+        .map(|line| {
+            line.parse()
+                .expect("A line in the input file is not a valid number")
+        })
         .collect();
 
     let first = numbers
         .iter()
         .tuple_combinations()
         .find(|&(a, b)| a + b == 2020)
-        .unwrap();
+        .expect("Cannot find a combination of two numbers that return 2020");
 
     let second = numbers
         .iter()
         .tuple_combinations()
         .find(|&(a, b, c)| a + b + c == 2020)
-        .unwrap();
+        .expect("Cannot find a combination of three numbers that return 2020");
 
     println!("The solution for the first part is {0}", first.0 * first.1);
 
