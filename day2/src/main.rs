ad19beb5a9b7a1514e715a5f696e05a0756cec7e
Giorgio Pomettini <giorgio.freespire@gmail.com>
2020-12-02 15:32:08 +0000

    Made code a bit more defensive

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -2,16 +2,17 @@
 extern crate lazy_static;
 
 use regex::Regex;
+use std::any::TypeId;
 
 #[macro_export]
 macro_rules! lazy_capture_name {
     ($capture:expr, $name:expr, $type:ty) => {
         $capture
             .name($name)
-            .unwrap()
+            .expect(&format!("Cannot find label with name: {}", $name)[..])
             .as_str()
             .parse::<$type>()
-            .unwrap();
+            .expect(&format!("Cannot cast label to type: {:?}", TypeId::of::<$type>())[..]);
     };
 }
 
@@ -19,7 +20,7 @@ fn main() {
     lazy_static! {
         static ref RE: Regex =
             Regex::new(r"(?P<min>[0-9]+)-(?P<max>[0-9]+)\s(?P<letter>[a-z]):\s(?P<password>.*)")
-                .unwrap();
+                .expect("Invalid regex");
     }
 
     let input = include_str!("input.txt");
@@ -48,15 +49,17 @@ fn main() {
     total_occurrences = 0;
 
     for password in &passwords {
-        let capture = RE.captures(password).unwrap();
+        let capture = RE.captures(password).expect(&format!("Invalid capture for {}", password));
 
         let min = lazy_capture_name!(capture, "min", usize);
         let max = lazy_capture_name!(capture, "max", usize);
         let letter = lazy_capture_name!(capture, "letter", char);
         let password: String = lazy_capture_name!(capture, "password", String);
 
-        if (password.chars().nth(min - 1).unwrap() == letter)
-            ^ (password.chars().nth(max - 1).unwrap_or_default() == letter)
+        let first_letter = password.chars().nth(min - 1).expect("Index is out of bounds");
+        let second_letter = password.chars().nth(max - 1).expect("Index is out of bounds");
+
+        if (first_letter == letter) ^ (second_letter == letter)
         {
             total_occurrences += 1;
         }
