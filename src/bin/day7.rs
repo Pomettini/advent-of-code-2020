ad7fdc7f021832bc491ce587ea92acfdb545aa12
Giorgio Pomettini <giorgio.freespire@gmail.com>
2020-12-07 23:22:52 +0100

    Added first solution for day 7

--- src/bin/day7.rs
+++ src/bin/day7.rs
@@ -11,7 +11,7 @@ lazy_static! {
 fn main() {
     let input = include_str!("../../inputs/day7.txt");
 
-    let bags: HashMap<&str, HashMap<String, usize>> =
+    let bags: HashMap<String, HashMap<String, usize>> =
         input.lines().fold(HashMap::new(), |mut map, line| {
             let split: Vec<&str> = line.split(" bags contain ").collect();
             let bag = RE
@@ -19,21 +19,31 @@ fn main() {
                 .clone()
                 .split(", ")
                 .into_iter()
-                .fold(HashMap::new(), |mut a, b| {
-                    a.insert(
-                        b.chars().skip(2).into_iter().collect(),
-                        b.chars()
+                .filter(|x| !x.contains("no other"))
+                .fold(HashMap::new(), |mut map, x| {
+                    map.insert(
+                        x.chars().skip(2).into_iter().collect(),
+                        x.chars()
                             .next()
                             .unwrap()
                             .to_string()
                             .parse()
                             .unwrap_or_default(),
                     );
-                    a
+                    map
                 });
-            map.insert(split[0], bag);
+            map.insert(split[0].to_owned(), bag);
             map
         });
 
-    dbg!(bags);
+    fn can_hold(
+        bags: &HashMap<String, HashMap<String, usize>>,
+        bag: &HashMap<String, usize>,
+    ) -> bool {
+        bag.contains_key("shiny gold") || bag.keys().any(|b| can_hold(bags, bags.get(&*b).unwrap()))
+    }
+
+    let result: usize = bags.values().filter(|bag| can_hold(&bags, bag)).count();
+
+    println!("The solution for the first problem is {}", result);
 }
