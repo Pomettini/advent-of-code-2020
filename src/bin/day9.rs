a324deb7673a6871afa215f839e6a76340aa65ba
Giorgio Pomettini <giorgio.freespire@gmail.com>
2020-12-10 00:55:13 +0100

    Added crappy solution for second problem of day 9

--- src/bin/day9.rs
+++ src/bin/day9.rs
@@ -1,38 +1,51 @@
 fn main() {
     let input = include_str!("../../inputs/day9.txt");
 
-    let mut preamble_list: Vec<usize> = input
-        .lines()
-        .take(25)
-        .map(|line| line.parse().unwrap())
-        .collect();
+    let lines: Vec<usize> = input.lines().map(|line| line.parse().unwrap()).collect();
 
-    fn is_preamble(list: &Vec<usize>, number: usize) -> bool {
-        list.into_iter()
-            .find(|&&a| {
-                list.into_iter()
-                    .find(|&&b| a != b && a + b == number)
-                    .is_some()
-            })
-            .is_some()
+    let mut preamble_list: Vec<usize> = lines.iter().take(25).map(|l| l.to_owned()).collect();
+
+    fn is_not_preamble(list: &[usize], number: usize) -> bool {
+        list.iter()
+            .find(|&&a| list.iter().any(|&b| a != b && a + b == number))
+            .is_none()
     }
 
-    let first: usize = input
+    let first: (usize, usize) = input
         .lines()
         .skip(25)
         .map(|line| line.parse::<usize>().unwrap())
-        .filter(|number| {
-            let result = !is_preamble(&preamble_list, *number);
+        .enumerate()
+        .filter(|(_i, number)| {
+            let result = is_not_preamble(&preamble_list, *number);
             preamble_list.remove(0);
             preamble_list.push(*number);
             result
         })
-        .collect::<Vec<usize>>()
+        .collect::<Vec<(usize, usize)>>()
         .first()
         .unwrap()
         .to_owned();
 
-    println!("The solution for the first problem is {}", first);
+    println!("The solution for the first problem is {}", first.1);
 
-    assert!(first == 90433990);
+    assert!(first.1 == 90433990);
+
+    let second: usize = (0..first.0)
+        .into_iter()
+        .find_map(|skip| {
+            (0..100).into_iter().find_map(|take| {
+                let range: Vec<usize> = lines.clone().into_iter().skip(skip).take(take).collect();
+                if range.clone().into_iter().sum::<usize>() == first.1 {
+                    Some(range.iter().min().unwrap() + range.iter().max().unwrap())
+                } else {
+                    None
+                }
+            })
+        })
+        .unwrap();
+
+    println!("The solution to the second problem is {}", second);
+
+    assert!(second == 11691646);
 }
